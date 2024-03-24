5a93e9bb6764e844065ac6dd460849db7be8034f
kasshii28 <gp22a043@oecu.jp>
2024-03-24 16:37:58 +0900

    型変換まで

---
+++ src/9.interface&nil/main.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+//interface & nil
+
+func main(){
+	var x interface{}
+	fmt.Println(x)
+
+	x = 1.1
+	fmt.Println(x)
+	x = "A"
+	fmt.Println(x)
+	x = [3]int{1,2,3}
+	fmt.Println(x)
+
+	x = 2
+	fmt.Println(x + 3)
+}
